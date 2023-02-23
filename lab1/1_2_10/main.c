49003b4362101702cef3d9eb7188faf44bd066ce
BartekKow <115708938+BartekKow@users.noreply.github.com>
2023-02-23 17:35:56 +0100

    add 1.2.10

--- lab1/1_2_10/main.c
+++ lab1/1_2_10/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
+    double x=-9.098;
+    printf("Podaj liczbe: ");
+    scanf("%lf", &x);
+    printf("%lf",fabs(x));
 }
