c49f9d98275009315e5ce7d982d5d9b36e66d913
BartekKow <115708938+BartekKow@users.noreply.github.com>
2023-02-23 16:02:20 +0100

    add 1.2.2 to 1.2.9

---
+++ lab1/1_2_6/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c;
+    printf("Podaj liczby: ");
+    scanf("%d %d %d",&a,&b,&c);
+    printf("%d\n%d\n%d",a,b,c);
+    return 0;
+}
---
+++ lab1/1_2_9/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float x;
+    scanf("%fl",&x);
+    printf("%fl",sqrt(x));
+}
