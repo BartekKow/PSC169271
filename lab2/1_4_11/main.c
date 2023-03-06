ab0d920505b214ca4b787756d2f66b076988eb13
BartekKow <115708938+BartekKow@users.noreply.github.com>
2023-03-06 15:25:14 +0100

    add 1.4.11

--- lab2/1_4_11/main.c
+++ lab2/1_4_11/main.c
@@ -5,18 +5,103 @@ int main()
 {
     double a,b,c,d;
     scanf("%lf %lf %lf %lf",&a,&b,&c,&d);
+
     //a)
     int x=0;
-    while((abs(a)*(x*x)+b*x+c)<d)
+    double y=abs(a)*(x*x)+b*x+c;
+    if(a==0 && b==0 && c==0 && d==0)
     {
-        x++;
+        printf("a) Brak rozwiazan \n");
     }
-    printf("a) %d\n", x);
+    else if(y<=d)
+    {
+        while((abs(a)*(x*x)+b*x+c)<=d)
+            {
+                x++;
+            }
+        printf("a) %d\n", x);
+    }
+    else
+    {
+        printf("a) %d\n", x);
+    }
+
     //b)
     x=0;
-    while(5*(x*x)+a*x+b<c)
+    y=5*(x*x)+a*x+b;
+    if(y<c)
+    {
+        while(5*(x*x)+a*x+b<c)
+        {
+            x++;
+        }
+        printf("b) %d\n", x-1);
+    }
+    else if(y>c && a<0)
+    {
+        while(5*(x*x)+a*x+b>c)
+        {
+            x++;
+        }
+        printf("b) %d\n", x);
+    }
+    else if(y==c)
     {
         x++;
+        if(5*(x*x)+a*x+b<c)
+        {
+            while(5*(x*x)+a*x+b<c)
+            {
+                x++;
+            }
+            printf("b) %d\n", x-1);
+        }
+        else if(5*(x*x)+a*x+b>c && a<0)
+        {
+            while(5*(x*x)+a*x+b>c)
+            {
+                x++;
+            }
+            printf("b) %d\n", x);
+        }
     }
-    printf("%db)", x);
+
+    //c)
+    x=0;
+    if(y<c)
+    {
+        while(5*(x*x)+a*x+b<=c)
+        {
+            x++;
+        }
+        printf("c) %d\n", x-1);
+    }
+    else if(y>c && a<0)
+    {
+        while(5*(x*x)+a*x+b>=c)
+        {
+            x++;
+        }
+        printf("c) %d\n", x);
+    }
+    else if(y==c)
+    {
+        x++;
+        if(5*(x*x)+a*x+b<c)
+        {
+            while(5*(x*x)+a*x+b<=c)
+            {
+                x++;
+            }
+            printf("c) %d\n", x-1);
+        }
+        else if(5*(x*x)+a*x+b>c && a<0)
+        {
+            while(5*(x*x)+a*x+b>=c)
+            {
+                x++;
+            }
+            printf("c) %d\n", x);
+        }
+    }
 }
