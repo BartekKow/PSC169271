c01d018266930c85614274f463e35d36266facdc
BartekKow <115708938+BartekKow@users.noreply.github.com>
2023-04-06 15:22:36 +0200

    update

--- lab6/4_2_8/main.c
+++ lab6/4_2_8/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void foo(int n, int tab1[], int tab2[], double tab3[])
+void foo(int n, double tab1[], double tab2[], double tab3[])
 {
     for(int i=0; i<n; i++)
     {
@@ -12,8 +12,8 @@ void foo(int n, int tab1[], int tab2[], double tab3[])
 
 int main()
 {
-    int tab[4]={1,2,3,4};
-    int tab2[4]={5,6,7,8};
+    double tab[4]={1,2,3,4};
+    double tab2[4]={5,6,7,8};
     double tab3[8];
     foo(4,tab,tab2,tab3);
     return 0;
