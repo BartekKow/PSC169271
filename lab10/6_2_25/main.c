310cc85e4f24c86dd3b1fd52b565eba24abfd99f
BartekKow <115708938+BartekKow@users.noreply.github.com>
2023-05-01 17:18:42 +0200

    add

--- lab10/6_2_25/main.c
+++ lab10/6_2_25/main.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void foo(int ***tab, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i+1; j < n; j++)
-            {
-                int temp = (*tab)[i][j];
-                (*tab)[i][j] = (*tab)[j][i];
-                (*tab)[j][i] = temp;
-            }
+void foo(int** tab, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            int temp = tab[i][j];
+            tab[i][j] = tab[j][i];
+            tab[j][i] = temp;
+        }
     }
 }
 
 int main() {
-    int n = 3;
-    int** tab = malloc(n * sizeof(int*));
+    int n = 4;
+    int** tab = (int**)malloc(n * sizeof(int*));
     for (int i = 0; i < n; i++) {
-        tab[i] = malloc(n * sizeof(int));
+        tab[i] = (int*)malloc(n * sizeof(int));
         for (int j = 0; j < n; j++) {
-            tab[i][j] = i*n+j+1;
+            tab[i][j] = i * n + j;
         }
     }
-    printf("Przed zmiana miejscami:\n");
+    foo(tab, n);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             printf("%d ", tab[i][j]);
@@ -31,14 +28,10 @@ int main() {
         printf("\n");
     }
 
-    foo(&tab, n);
-
-    printf("Po zmianie miejscami:\n");
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("%d ", tab[i][j]);
-        }
-        printf("\n");
+        free(tab[i]);
     }
+    free(tab);
+
     return 0;
 }
